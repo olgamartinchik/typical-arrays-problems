422545dbb199dd2216db99070c87fe57334a432f
olgamartinchik <sintcerova@mail.ru>
2020-10-01 10:58:15 +0300

    feat: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let result
+    if (typeof array !== 'object' || array.length == 0) {
+        return 0;
+    } else if (array) {
+        result = Math.min.apply(Math, array);
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
 
-exports.avg = function avg (array) {
-  return 0;
+exports.max = function max(array) {
+    let result
+    if (typeof array !== 'object' || array.length == 0) {
+        return 0;
+    } else if (array) {
+        result = Math.max.apply(Math, array);
+    }
+    return result;
+}
+exports.avg = function avg(array) {
+    let result
+    if (typeof array !== 'object' || array.length == 0) {
+        return 0;
+    } else {
+
+        let sum = array.reduce((a, b) => (a + b), 0);
+        result = sum / array.length;
+    }
+
+    return result;
 }
